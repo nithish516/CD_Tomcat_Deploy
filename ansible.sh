350e27a4472ea957252fdf8aea4189a3f2451e82
mohangopineni <gopineni.mohan@gmail.com>
2017-08-08 19:41:04 -0700

    inven

--- ansible.sh
+++ ansible.sh
@@ -4,4 +4,4 @@ echo "[$1]" >> ./inventory
 /root/.local/bin/aws ec2 describe-instances | grep PublicIpAddress | awk -F ":" '{print $2}' | sed -e 's/[",]//g' > ./ipaddress.txt
 cat ipaddress.txt | sed 's/^[ \t]*//;s/[ \t]*$//' >> ./ipaddress1.txt
 tail -1 ipaddress1.txt >> ./inventory
-ansible-playbook site.yml --limit $1
+ansible-playbook -i ./inventory site.yml --limit $1

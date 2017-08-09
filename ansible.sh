b5d4b0afe4a18e530b036e310276ccb72049aa95
mohangopineni <gopineni.mohan@gmail.com>
2017-08-08 20:13:00 -0700

    tail

--- ansible.sh
+++ ansible.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 chmod 0400 ./AmazonInstances.pem
-cat ./inventory
 echo "[$1]" >> ./inventory
+rm -f ./ipaddress.txt ./ipaddress.txt
 /root/.local/bin/aws ec2 describe-instances | grep PublicIpAddress | awk -F ":" '{print $2}' | sed -e 's/[",]//g' > ./ipaddress.txt
 cat ./ipaddress
 cat ipaddress.txt | sed 's/^[ \t]*//;s/[ \t]*$//' >> ./ipaddress1.txt
 cat ./ipaddress1.txt
-tail -1 ipaddress1.txt >> ./inventory
+tail -1 ./ipaddress1.txt >> ./inventory
 cat ./inventory
 ansible-playbook -i ./inventory site.yml --limit $1
